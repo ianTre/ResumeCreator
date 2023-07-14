81fec13e53dc7273f38467b0d6abed5e02824bef
ianTre <ianntrejo96@gmail.com>
2023-07-14 19:46:57 -0300

    Add Account Manager

---
+++ Manager/AccountManager.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using ResumeCreator.Models;
+
+namespace ResumeCreator.Manager
+{
+    public class AccountManager : IAccountManager
+    {
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Manager/IAccountManager.cs
@@ -0,0 +1,9 @@
+﻿using ResumeCreator.Models;
+
+namespace ResumeCreator.Manager
+{
+    public interface IAccountManager
+    {
+        public void Add(User user);
+    }
+}
