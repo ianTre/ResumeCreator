b030d672a17c96620af1672bc151f249f5fd8266
DigitalBoy1981 <maurokucich@hotmail.com>
2023-07-24 21:00:19 -0300

    Controlador y vista de Usuario Nuevo.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeCreator.Manager;
+using ResumeCreator.Models;
+
+namespace ResumeCreator.Controllers
+{
+    public class AccountController : Controller
+    {
+        private AccountManager _Manager;
+
+        public AccountController()
+        {
+            _Manager = new AccountManager();
+
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult New(User user)
+        {
+                
+
+            _Manager.Add(user);
+
+            //Retornar a la vista de listado
+            return RedirectToAction("ProfileDataList","ProfileData");
+        }
+
+
+    }
+}
